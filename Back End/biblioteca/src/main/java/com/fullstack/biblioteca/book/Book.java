4d57f05901ba915ae560f06382ce328827ac0bd0
Rvictorio <rvictorio8@gmail.com>
2024-07-26 12:15:12 -0300

    Preparação para apagar livros e preparação para atualização de dados

--- Back End/biblioteca/src/main/java/com/fullstack/biblioteca/book/Book.java
+++ Back End/biblioteca/src/main/java/com/fullstack/biblioteca/book/Book.java
@@ -3,7 +3,7 @@ package com.fullstack.biblioteca.book;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id; // Corrigido
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -27,5 +27,37 @@ public class Book {
         this.image = data.image();
         this.price = data.price();
         this.title = data.title();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
     }
 }
--- Back End/biblioteca/src/main/java/com/fullstack/biblioteca/controller/BookController.java
+++ Back End/biblioteca/src/main/java/com/fullstack/biblioteca/controller/BookController.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+
 @RestController
-@RequestMapping("book")
+@RequestMapping("/book")
 public class BookController {
 
     @Autowired
@@ -20,14 +22,33 @@ public class BookController {
     public void saveBook(@RequestBody BookRequestDTO data){
         Book bookData = new Book(data);
         repository.save(bookData);
-        return;
     }
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
     public List<BookResponseDTO> getAll(){
-
         List<BookResponseDTO> bookList = repository.findAll().stream().map(BookResponseDTO::new).toList();
         return bookList;
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PutMapping("/{id}")
+    public BookResponseDTO updateBook(@PathVariable Long id, @RequestBody BookRequestDTO data) {
+        Optional<Book> optionalBook = repository.findById(id);
+        if (optionalBook.isPresent()) {
+            Book book = optionalBook.get();
+            book.setTitle(data.title());
+            book.setImage(data.image());
+            book.setPrice(data.price());
+            repository.save(book);
+            return new BookResponseDTO(book);
+        }
+        throw new RuntimeException("Book not found with id " + id);
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        repository.deleteById(id);
     }
 }
